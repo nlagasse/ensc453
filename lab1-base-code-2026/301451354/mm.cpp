d1fef8129f4df309d0357959d9845a94c1f7a944
Nicholas Lagasse <nlagasse12@icloud.com>
2026-01-27 16:58:08 -0800

    added intrinsics and vectorization

--- lab1-base-code-2026/301451354/mm.cpp
+++ lab1-base-code-2026/301451354/mm.cpp
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include "my_timer.h"
 
+#include <x86intrin.h>
 #include <omp.h>
+#include <math.h> 
+
 
 #define NI 2048
 #define NJ 2048
@@ -53,6 +56,15 @@ void print_array_sum(float C[NI*NJ])
   printf("sum of C array = %f\n", sum);
 }
 
+//helper function for transposing a matrix
+float* transposeMatrix(const float B[NK*NJ], float transposed[NK*NJ]) {
+    for (int i = 0; i < NK; i++) {
+        for (int j = 0; j < NJ; j++) {
+            transposed[j*NI + i] = B[i*NJ + j];
+        }
+    }
+  return transposed;
+}
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
@@ -69,25 +81,61 @@ void kernel_gemm(float C[NI*NJ], float A[NI*NK], float B[NK*NJ], float alpha, fl
 // printf("number of devices = %d\n", omp_get_max_threads());
 
 // Tiling
-int TILE_SIZE = 16;
+int TILE_SIZE_i = 16;
+int TILE_SIZE_j = 16;
+int TILE_SIZE_k = 2048;
 
-omp_set_num_threads(6);
-#pragma omp parallel for private (j)
-  for (i = 0; i < NI; i+=TILE_SIZE) {
-    // J
-    for (j = 0; j < NJ; j+=TILE_SIZE) {
-      // I
-      for(int row = i; row < TILE_SIZE +i; row++){
-        for(int col = j; col < TILE_SIZE +j; col++){
+__m256 A_row ;
+__m256 B_row;
+__m256 alpha_vec;
+
+omp_set_num_threads(20);
+
+float *B_trans = (float*)malloc(NK*NJ*sizeof(float));
+transposeMatrix(B, B_trans);
+
+#pragma omp parallel for
+  for (i = 0; i < NI; i+=TILE_SIZE_i) {
+    for (j = 0; j < NJ; j+=TILE_SIZE_j) {
+      for(int row = i; row < TILE_SIZE_i +i; row++){
+        for(int col = j; col < TILE_SIZE_j +j; col++){
           C[row*NJ+col] *= beta;
         }
       }
       // K 
-      for(k = 0; k< NK; k+= TILE_SIZE){
-        for (ii=i; ii< TILE_SIZE+i && ii<NI; ii++){
-          for (jj=j; jj< TILE_SIZE+j && jj<NJ; jj++){              
-            for (kk=k; kk< TILE_SIZE+k && kk<NK; kk++){ 
-              C[ii*NJ+jj] += alpha * A[ii*NK+kk] * B[kk*NJ+jj];
+      for(k = 0; k< NK; k+= TILE_SIZE_k){
+        for (ii=i; ii< TILE_SIZE_i+i && ii<NI; ii++){
+          for (jj=j; jj< TILE_SIZE_i+j && jj<NJ; jj++){
+
+            __m256 sum = _mm256_setzero_ps();           
+            
+            for (kk=k; kk< TILE_SIZE_k+k; kk+=16){
+
+              __m256 alpha_vec = _mm256_set1_ps(alpha);
+
+              __m256 A_row_1 = _mm256_loadu_ps(&A[ii*NK+kk]);
+              __m256 B_row_1 = _mm256_loadu_ps(&B_trans[jj*NJ+kk]); 
+
+              __m256 A_row_2 = _mm256_loadu_ps(&A[ii*NK+kk+8]);
+              __m256 B_row_2 = _mm256_loadu_ps(&B_trans[jj*NJ+kk+8]); 
+
+              sum =_mm256_add_ps(sum, _mm256_mul_ps((_mm256_mul_ps(alpha_vec, A_row_1)), B_row_1));
+              sum =_mm256_add_ps(sum, _mm256_mul_ps((_mm256_mul_ps(alpha_vec, A_row_2)), B_row_2));                      
+              // C[ii*NJ+jj] += alpha * A[ii*NK+kk] * B[kk*NJ+jj];
+            }
+
+            float tempResult[8];
+            _mm256_storeu_ps(tempResult, sum);
+
+            for (int index = 0; index < 1; index++) {
+              C[ii*NJ+jj] += tempResult[index];
+              C[ii*NJ+jj] += tempResult[index + 1];
+              C[ii*NJ+jj] += tempResult[index + 2];
+              C[ii*NJ+jj] += tempResult[index + 3];
+              C[ii*NJ+jj] += tempResult[index + 4];
+              C[ii*NJ+jj] += tempResult[index + 5];
+              C[ii*NJ+jj] += tempResult[index + 6];
+              C[ii*NJ+jj] += tempResult[index + 7];
             }
           }
         }
